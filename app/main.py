b52ab04935a5038139f6f1443dec2d8de175ad40
Stephen Stucky <sstucky@guidewire.com>
2024-10-25 09:28:26 -0500

    response with just OK when we receive data

--- app/main.py
+++ app/main.py
@@ -6,7 +6,13 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client, addr = server_socket.accept() # wait for client
+
+    # get a message from the client
+    data = client.recv(4096)
+
+    # send a message to the client
+    client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
